0fae9676fbfc2dbb8bc58feb091d83e71dde9bcd
CryptoSObe <seansobol@gmail.com>
2022-03-27 14:35:36 -0500

    ready for polygon upload

--- backend/src/config.js
+++ backend/src/config.js
@@ -51,8 +51,8 @@ const CONTRACT_SYMBOL = 'WORM';
 const METADATA_UPDATABLE = true; // set to false if you don't want to allow metadata updates after minting
 const OWNER_ADDRESS = '0x7d59C6160c9c72bD22cd35E43F7B4359B519173A';
 const TREASURY_ADDRESS = '0x7d59C6160c9c72bD22cd35E43F7B4359B519173A';
-const MAX_SUPPLY = 5000; // The maximum number of NFTs that can be minted. CANNOT BE UPDATED!
-const MINT_PRICE = .001; // Minting price per NFT. Rinkeby = ETH, Polygon = MATIC. CANNOT BE UPDATED!
+const MAX_SUPPLY = 100; // The maximum number of NFTs that can be minted. CANNOT BE UPDATED!
+const MINT_PRICE = 9; // Minting price per NFT. Rinkeby = ETH, Polygon = MATIC. CANNOT BE UPDATED!
 const TOKENS_PER_MINT = 10; // maximum number of NFTs a user can mint in a single transaction. CANNOT BE UPDATED!
 
 // REQUIRED CONTRACT DETAILS THAT CAN BE UPDATED LATER.
